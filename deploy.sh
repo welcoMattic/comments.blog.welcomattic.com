77bd12fb74e358bbd400aaf22d3983aafbad2c06
Mathieu Santostefano <msantostefano@protonmail.com>
2022-08-02 15:50:24 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+curl https://github.com/umputun/remark42/releases/download/v1.10.1/remark42.linux-amd64.tar.gz --output remark42.linux-amd64.tar.gz
+tar -xvf remark42.linux-amd64.tar.gz 
+cd remark42.linux-amd64 
+./remark42.linux-amd64 server
