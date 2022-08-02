ff9b0f60cf8fb6917403a0eee4eb4dc248236864
Mathieu Santostefano <msantostefano@protonmail.com>
2022-08-02 16:09:50 +0200

    Fix remark download

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-curl https://github.com/umputun/remark42/releases/download/v1.10.1/remark42.linux-amd64.tar.gz --output remark42.linux-amd64.tar.gz
+curl -L https://github.com/umputun/remark42/releases/download/v1.10.1/remark42.linux-amd64.tar.gz --output remark42.linux-amd64.tar.gz
 tar -xvf remark42.linux-amd64.tar.gz 
-cd remark42.linux-amd64 
 ./remark42.linux-amd64 server
